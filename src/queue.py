4d8aa0397cb161455126c87d1b129891ced146fe
Serov Ivan <serovvans@yandex.ru>
2023-08-07 19:15:19 +0300

    add Queue class

--- src/queue.py
+++ src/queue.py
@@ -7,7 +7,8 @@ class Node:
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Queue:
@@ -15,7 +16,8 @@ class Queue:
 
     def __init__(self):
         """Конструктор класса Queue"""
-        pass
+        self.head = None
+        self.tail = None
 
     def enqueue(self, data):
         """
@@ -23,7 +25,14 @@ class Queue:
 
         :param data: данные, которые будут добавлены в очередь
         """
-        pass
+        if self.head is None:
+            self.tail = Node(data, None)
+            self.head = self.tail
+        else:
+            new = Node(data, None)
+            self.tail.next_node = new
+            self.tail = new
+
 
     def dequeue(self):
         """
@@ -31,8 +40,15 @@ class Queue:
 
         :return: данные удаленного элемента
         """
-        pass
 
     def __str__(self):
         """Магический метод для строкового представления объекта"""
-        pass
+        if self.head is not None:
+            data = [self.head.data]
+            curr = self.head
+            while curr.next_node is not None:
+                curr = curr.next_node
+                data.append(curr.data)
+        else:
+            data = []
+        return "\n".join(data)
--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,1 +1,26 @@
 """Здесь надо написать тесты с использованием unittest для модуля queue."""
+from src.queue import Queue
+
+
+def test_enqueue():
+    queue = Queue()
+    queue.enqueue('data1')
+    queue.enqueue('data2')
+    queue.enqueue('data3')
+
+    assert queue.head.data == 'data1'
+    assert queue.head.next_node.data == 'data2'
+    assert queue.tail.data == 'data3'
+    assert queue.tail.next_node is None
+
+
+def test_str():
+    queue = Queue()
+
+    assert str(Queue()) == ""
+
+    queue.enqueue('data1')
+    queue.enqueue('data2')
+    queue.enqueue('data3')
+
+    assert str(queue) == "data1\ndata2\ndata3"
