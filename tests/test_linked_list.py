f06589e796b0aa4952da934429d4185954c0f02c
Serov Ivan <serovvans@yandex.ru>
2023-08-26 00:14:54 +0300

    add to_list and get_by_id methods

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -13,3 +13,16 @@ def test_linked_list():
 
     # Печатаем данные
     assert str(ll) == "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None"
+
+
+def test_to_list(ll):
+    lst = ll.to_list()
+    assert lst[0] == {'id': 0, 'username': 'serebro'}
+    assert lst[1] == {'id': 1, 'username': 'lazzy508509'}
+    assert lst[2] == {'id': 2, 'username': 'mik.roz'}
+    assert lst[3] == {'id': 3, 'username': 'mosh_s'}
+
+
+def test_by_id(ll):
+    user_data = ll.get_data_by_id(3)
+    assert user_data == {'id': 3, 'username': 'mosh_s'}
